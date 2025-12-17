348e1a61c5e5a4e64545e10ae7038c4f2a68760d
David Browning <david.a.browning@gmail.com>
2025-12-17 13:53:47 +0100

    Refactor tests.

--- ConferenceBooking.ServicesTests/BookingServiceTests.cs
+++ ConferenceBooking.ServicesTests/BookingServiceTests.cs
@@ -20,9 +20,9 @@ namespace ConferenceBooking.ServicesTests
         {
             // Arrange
             int expectedCount = _bookingService.GetAllBookings().Count() + 1;
-            RoomDto roomDto = new() { Name = "Room1" };
-            _bookingService.AddRoom(roomDto);
-            roomDto = _bookingService.GetRoomByName("Room1");
+            string roomName = "Room1";
+            _bookingService.AddRoom(new RoomDto() { Name = roomName });
+            RoomDto roomDto = _bookingService.GetRoomByName("Room1");
             BookingDto bookingDto = new() { RoomId = roomDto.Id };
 
             // Act
@@ -43,8 +43,8 @@ namespace ConferenceBooking.ServicesTests
             _bookingService.AddRoom(new RoomDto() { Name = room2Name });
             RoomDto roomDto1 = _bookingService.GetRoomByName(room1Name);
             RoomDto roomDto2 = _bookingService.GetRoomByName(room2Name);
-            _bookingService.AddBooking(new BookingDto { RoomId = roomDto1.Id });
-            _bookingService.AddBooking(new BookingDto { RoomId = roomDto1.Id });
+            for (int i = 0; i < expectedCount; i++)
+                _bookingService.AddBooking(new BookingDto { RoomId = roomDto1.Id });
             _bookingService.AddBooking(new BookingDto { RoomId = roomDto2.Id });
 
             // Act
