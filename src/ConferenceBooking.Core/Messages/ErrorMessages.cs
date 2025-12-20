ee40d8699bcfc082a5b06210d15338d6fc28ab47
David Browning <david.a.browning@gmail.com>
2025-12-20 21:28:37 +0100

    Add duplicate room name error messages.

--- src/ConferenceBooking.Core/Messages/ErrorMessages.cs
+++ src/ConferenceBooking.Core/Messages/ErrorMessages.cs
@@ -6,5 +6,6 @@
         public const string BookingIsNull = "Error: Booking is null.";
         public const string RoomIsBooked = "Error: Room is booked";
         public const string RoomIsNull = "Error: Room is null.";
+        public const string RoomNameAlreadyExists = "Error: Room name already exists.";
     }
 }
