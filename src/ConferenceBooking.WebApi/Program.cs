24261e06701422e61602e282ab765218232cecad
David Browning <david.a.browning@gmail.com>
2025-12-20 23:13:16 +0100

    Configure Swagger.

--- src/ConferenceBooking.WebApi/Program.cs
+++ src/ConferenceBooking.WebApi/Program.cs
@@ -18,10 +18,10 @@ namespace ConferenceBooking.WebApi
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "ConferenceBooking"));
             }
 
             app.UseHttpsRedirection();
