1a05945d2db36356348cbc39b3871b88bc448f94
David Browning <david.a.browning@gmail.com>
2025-12-21 15:10:54 +0100

    Add UI that fetches users from API and displays list in console app.

--- src/ConferenceBooking.ConsoleUI/Program.cs
+++ src/ConferenceBooking.ConsoleUI/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConferenceBooking.ConsoleUI
+﻿using ConferenceBooking.Core.Dtos;
+using Newtonsoft.Json;
+
+namespace ConferenceBooking.ConsoleUI
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HttpClient httpClient = new() { BaseAddress = new Uri("https://localhost:7180") };
+            var response = await httpClient.GetAsync($"api/applicationusers");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            IEnumerable<ApplicationUserDto> applicationUserDtos = JsonConvert.DeserializeObject<IEnumerable<ApplicationUserDto>>(content);
+
+            Console.WriteLine("User list:");
+            foreach (ApplicationUserDto applicationUserDto in applicationUserDtos)
+                Console.WriteLine($"{applicationUserDto.Id}. {applicationUserDto.Username}");
+            Console.WriteLine("Press ENTER exit.");
+            Console.ReadLine();
         }
     }
 }
